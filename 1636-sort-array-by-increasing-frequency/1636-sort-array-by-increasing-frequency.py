8da68f3d12f67b5f0fbce57678acc63d43a07336
Nathishwar <nathishwarc@gmail.com>
2026-08-16 11:13:16 +0530

    Time: 11 ms (17.22%), Space: 12.4 MB (62.64%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -5,9 +5,32 @@ class Solution(object):
         :rtype: List[int]
         """
         freq = {}
+        '''
+        freq = {}
+
         for num in nums:
             freq[num] = freq.get(num, 0) + 1
 
-        nums.sort(key=lambda x : (freq[x] , -x)) # this is pilot condition
+        nums.sort(key=lambda num: (freq[num], -num))
 
         return nums
+        '''
+        # Count frequency
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        # Create buckets
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for num, count in freq.items():
+            buckets[count].append(num)
+
+        result = []
+
+        # Lowest frequency first
+        for count in range(1, len(buckets)):
+            # Same frequency -> larger value first
+            for num in sorted(buckets[count], reverse=True):
+                result.extend([num] * count)
+
+        return result
