8eaad14693e2592ced84280f797fd031cac6fd1b
Nathishwar <nathishwarc@gmail.com>
2026-08-16 11:40:11 +0530

    Time: 26 ms (32.52%), Space: 13.2 MB (88.75%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+
+        for i in range(32):
+            bit_sum = 0
+
+            for num in nums:
+                bit_sum += (num >> i) & 1
+
+            if bit_sum % 3 != 0:
+                result |= (1 << i)
+
+        # Convert to Python's signed 32-bit integer
+        if result >= 2**31:
+            result -= 2**32
+
+        return result
